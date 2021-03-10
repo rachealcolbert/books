598ec06d1ef9c534cc5de3e5e1ca9cf7c1a0f826
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-10 00:42:08 -0700

    update const client declaration

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,15 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
   uri: "/graphql",
 });
 
