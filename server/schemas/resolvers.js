7bd6407d707468359ce650befff201c8193fa811
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-09 21:24:17 -0700

    login resolver updated with authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
@@ -13,7 +14,19 @@ const resolvers = {
         const user = await User.create(args);
         return user;
       },
-      login: async () => {},
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+          throw new AuthenticationError("Incorrect password");
+        }
+        return user;
+      },
     },
   },
 };
