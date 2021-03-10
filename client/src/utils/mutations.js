22c41a7320315143a2cba14ad9d3047643883209
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-10 00:10:21 -0700

    login user mutation added

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,19 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+ADD_USER;
+
+SAVE_BOOK;
+
+REMOVE_BOOK;
