701e600f9cb3de168f8343dfd1aaedd675066732
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-09 20:28:54 -0700

    apollo added to server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+//import Apolloserver
+const { ApolloServer } = require("apollo-server-express");
 
-const app = express();
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+//create new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
+//integrate Apollo sever with the Express application
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
