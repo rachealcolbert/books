f6b08d12c7bbe7b88ee9b814c6908a536bede992
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-10 20:32:56 -0700

    login form working better with no errors

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -9,6 +9,9 @@ import Auth from "../utils/auth";
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
   const [login, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
